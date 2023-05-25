280b10755c953170b27d0a6dded17403d342c17b
tima1 <tima1@TIIA>
2023-05-25 18:17:32 +0300

    Add project files.

---
+++ WebAppFirst/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Yhtiön perustietojen kuvailua.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Yhteystietojamme:";
+
+            return View();
+        }
+        public ActionResult Map()
+        {
+            ViewBag.Message = "Saapumisohje:";
+
+            return View();
+        }
+    }
+}
---
+++ WebAppFirst/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            northwindEntities db = new northwindEntities();
+            List<Products> model = db.Products.ToList();
+            db.Dispose();
+            return View(model);
+        }
+
+        public ActionResult Index2()
+        {
+            northwindEntities db = new northwindEntities();
+            List<Products> model = db.Products.ToList();
+            db.Dispose();
+            return View(model);
+        }
+    }
+}
